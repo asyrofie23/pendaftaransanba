626f48645c7fec7569b3ed3025bc0a83b6fc8a95
Pendaftaran <ahmadrifqi942987@gmail.com>
2025-06-03 14:46:18 +0700

    binding database

--- api/index.ts
+++ api/index.ts
@@ -1,34 +1,16 @@
-interface Env {Add commentMore actions
+interface Env {
   ASSETS: Fetcher;
+  DB: D1Database;
 }
 
 export default {
-  fetch(request, env) {
+  async fetch(request, env) {
     const url = new URL(request.url);
 
     if (url.pathname.startsWith("/api/events")) {
-      return Response.json([
-        {
-          id: '1',
-          name: "event 1",
-          place: "pandaan",
-          time: 1741247092,
-        },
-        {
-          id: '2',
-          name: "event 2",
-          place: "sukorejo",
-          time: 1741246092,
-        },
-        {
-          id: '3',
-          name: "event 3",
-          place: "purwosari",
-          time: 1741245092,
-        },
-      ]);
+      let { results } = await env.DB.prepare("SELECT * FROM events").all();Add commentMore actions
+      return Response.json(results);
     }
-
     return env.ASSETS.fetch(request);
-  },
+ },
 } satisfies ExportedHandler<Env>;
