d18d094d17c74154b6034cbedd3ce7b1b329c077
Pendaftaran <ahmadrifqi942987@gmail.com>
2025-06-17 14:29:15 +0700

    update dan delete data

--- api/index.ts
+++ api/index.ts
@@ -15,7 +15,34 @@ app.post("/api/pendaftaransanbas", async (c) => {
   const newId = crypto.randomUUID();
   const input = await c.req.json<any>();
   const query = `INSERT INTO users(id,name,phone,email,address,school,time) values ("${newId}","${input.name}","${input.phone}","${input.email}","${input.address}","${input.school}",${input.time})`;
-  const newEvent = await c.env.DB.exec(query);
-  return c.json(newEvent);
+  const newUser = await c.env.DB.exec(query);
+  return c.json(newUser);
+});
+
+app.get("/api/pendaftaransanbas/:id", async (c) => {
+  const userId = c.req.param("id");
+  let { results } = await c.env.DB.prepare("SELECT * FROM users WHERE id = ?")
+    .bind(userId)
+    .all();
+  return c.json(results[0]);
+});
+
+app.put("/api/pendaftaransanbas/:id", async (c) => {
+  const userId = c.req.param("id");
+
+  const input = await c.req.json<any>();
+  const query = `UPDATE users SET name = "${input.name}", phone = "${input.phone}", email = "${input.email}", address = "${input.address}", school = "${input.school}", time = ${input.time} WHERE id = "${userId}"`;
+  const user = await c.env.DB.exec(query);
+
+  return c.json(user);
+});
+
+app.delete("/api/pendaftaransanbas/:id", async (c) => {
+  const userId = c.req.param("id");
+
+  const query = `DELETE FROM users WHERE id = "${userId}"`;
+  const user = await c.env.DB.exec(query);
+
+  return c.json(user);
 });
 export default app;
